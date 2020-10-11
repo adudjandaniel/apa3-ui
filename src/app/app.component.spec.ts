550e9f0a2bc2708d93a5adf1e3c11ad65f9276ea
adudjandaniel <40501134+adudjandaniel@users.noreply.github.com>
2020-10-10 22:04:14 -0400

    initial web (#7)
    * initial web
    
    * undone
    
    * redo
    
    * removed useless test
    
    * removed e2e default test
    
    Co-authored-by: Daniel Adu-Djan <adudjand@msu.edu>

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('apa3-ui');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('apa3-ui app is running!');
-  });
 });
