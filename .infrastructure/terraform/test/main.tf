734ca2e61fcfb9764f0df2ee6194b92dfe3b42f3
Daniel Adu-Djan <adudjand@msu.edu>
2020-10-11 21:24:17 -0400

    removed acme

--- .infrastructure/terraform/test/main.tf
+++ .infrastructure/terraform/test/main.tf
@@ -71,31 +71,3 @@ resource "google_dns_record_set" "apa3-test" {
 resource "google_compute_address" "apa3_ui_vm_test_static_ip" {
   name = "apa3-ui-test-static-ip"
 }
-
-provider "acme" {
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
-
-resource "tls_private_key" "private_key" {
-  algorithm = "RSA"
-}
-
-resource "acme_registration" "reg" {
-  account_key_pem = tls_private_key.private_key.private_key_pem
-  email_address   = var.email_address
-}
-
-resource "acme_certificate" "certificate" {
-  account_key_pem           = acme_registration.reg.account_key_pem
-  common_name               = "apa3.test.msu.luzcode.com"
-  subject_alternative_names = []
-
-  dns_challenge {
-    provider = "gcloud"
-
-    config = {
-      GCE_PROJECT = var.project
-      GCE_SERVICE_ACCOUNT_FILE = var.credentials_file
-    }
-  }
-}
